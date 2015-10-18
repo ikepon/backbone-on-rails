9b693d5e01ee75c963b0d998a5098b653680c93c
ikepon <tatsuyanoheya@gmail.com>
2015-10-18 15:35:01 +0900

    Finished no.17 lesson of dotinstall backbone.js

--- app/assets/javascripts/tasks/views/tasks/task_view.js
+++ app/assets/javascripts/tasks/views/tasks/task_view.js
@@ -1,10 +1,38 @@
 TaskApp.TaskView = Backbone.View.extend({
   tagName: 'li',
+
+  initialize: function() {
+    this.model.on("destroy", this.remove, this);
+    this.model.on("change", this.render, this);
+  },
+
+  events: {
+    "click .delete": "destroy",
+    "click .toggle": "toggle"
+  },
+
   template: JST['tasks/task'],
 
   render: function() {
-    var template = this.template(this.model.toJSON());
+    var taskStatus;
+    this.model.get('completed') ? taskStatus = 'checked' : taskStatus = '';
+
+    var template = this.template(this.model.toJSON(), {taskStatus: taskStatus});
     this.$el.html(template);
     return this;
+  },
+
+  destroy: function() {
+    if (confirm("are you sure?")) {
+      this.model.destroy();
+    }
+  },
+
+  remove: function() {
+    this.$el.remove();
+  },
+
+  toggle: function() {
+    this.model.set('completed', !this.model.get('completed'));
   }
 });
