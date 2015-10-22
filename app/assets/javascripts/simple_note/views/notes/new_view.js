119e072732b9dbbeb9ecf2c664a613b7913ab10c
ikepon <tatsuyanoheya@gmail.com>
2015-10-22 23:47:18 +0900

    Bindings params

--- app/assets/javascripts/simple_note/views/notes/new_view.js
+++ app/assets/javascripts/simple_note/views/notes/new_view.js
@@ -7,10 +7,17 @@ s.Notes.NewView = Backbone.View.extend({
 
   template: JST["notes/new"],
 
+  bindings: {
+    "#input-note-title": "title",
+    "#input-note-raw_body": "raw_body"
+  },
+
   render: function() {
     this.$el.html(this.template(this.model.toJSON()));
 
     $("#main").html(this.el);
+
+    this.stickit();
 
     return this;
   }
