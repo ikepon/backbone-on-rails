7c60fe6c3cdfd0634571a220e05cded5a4c70e7e
ikepon <tatsuyanoheya@gmail.com>
2015-11-02 23:01:54 +0900

    Add raw_body to Style

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151101093511) do
+ActiveRecord::Schema.define(version: 20151102140037) do
 
   create_table "notes", force: :cascade do |t|
     t.string   "title"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20151101093511) do
     t.text     "body",       null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text     "raw_body"
   end
 
   create_table "tasks", force: :cascade do |t|
